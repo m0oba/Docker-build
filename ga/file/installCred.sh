ec222adca4f8b926dce12c5f6e1c306ea021f82f
m0oba <39407703+m0oba@users.noreply.github.com>
2021-03-27 22:32:17 +0700

    Update installCred.sh

--- ga/file/installCred.sh
+++ ga/file/installCred.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# # get client_secret.json
-# echo "Paste the client_secret.json here:"
-# read secret
-# [ ! -z "$secret" ] && echo $secret > /root/client_secret.json
+ # get client_secret.json
+ echo "Paste the client_secret.json here:"
+ read secret
+ [ ! -z "$secret" ] && echo $secret > /root/client_secret.json
 
-# echo ' '
-# install credentials
+ echo ' '
+ install credentials
 
 /env/bin/google-oauthlib-tool \
-	--client-secrets /root/file/client_secret_638044564124-jlc2o348mhkk1l0u0hkkag2utak4mvmo.apps.googleusercontent.com.json \
+	--client-secrets /root/client_secret.json \
 	--scope https://www.googleapis.com/auth/assistant-sdk-prototype \
 	--save --headless
 
